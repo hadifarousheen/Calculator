90440cad96a599dffe5bc5b2849fe20e08ce2f11
Hadifa Rousheen <93543742+hadifarousheen@users.noreply.github.com>
2024-09-18 19:19:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const input = document.querySelector(".input");
+const buttons = document.querySelectorAll("button");
+const specialchars = ["%", "*", "/", "-", "+", "="];
+let output = "";
+const calculate = (btnvalue) => {
+  if (btnvalue === "=" && output !== "") {
+    output = eval(output.replace("%", "/100"));
+  } else if (btnvalue === "AC") {
+    output = "";
+  } else if (btnvalue === "DEL") {
+    output = output.toString().slice(0, -1);
+  } else {
+    if (output === "" && specialchars.includes(btnvalue)) return;
+    output += btnvalue;
+  }
+  input.value = output;
+};
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+});
